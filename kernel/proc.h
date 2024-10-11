7878f363ab25648016df5fd3123d22cbe79f04a1
TonyNee <neetony@qq.com>
2024-10-11 20:45:00 +0800

    Add lab02-syscall files

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,3 +1,5 @@
+#define MASK_SIZE   24
+
 // Saved registers for kernel context switches.
 struct context {
   uint64 ra;
@@ -103,4 +105,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  char mask[MASK_SIZE];               // Trace Mask
 };
